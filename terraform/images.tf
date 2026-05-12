a58dc2322254dedf74d1f136b83eadfab6881965
s-sigalingging <silveriussigalingging17@gmail.com>
2026-05-12 15:49:51 +0700

    deploy pfsense

--- terraform/images.tf
+++ terraform/images.tf
@@ -1,7 +1,7 @@
 resource "google_compute_image" "pfsense_custom" {
   name = "pfsense-custom-image"
   raw_disk {
-    source = "https://storage.googleapis.com/cloud-lab-tf-state/pfSense-CE-memstick-serial-2.7.0-RELEASE-amd64.img.tar"
+    source = "https://storage.googleapis.com/cloud-lab-tf-state/pfSense-CE-memstick-serial-2.5.0-RELEASE-amd64.img.tar.gz"
   }
   guest_os_features {
     type = "UEFI_COMPATIBLE"
