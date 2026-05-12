70b0274d237c6de6a0a1269bd59b37562c93a9b7
s-sigalingging <silveriussigalingging17@gmail.com>
2026-05-12 22:33:06 +0700

    add attached disk

--- terraform/pfsense.tf
+++ terraform/pfsense.tf
@@ -1,3 +1,10 @@
+resource "google_compute_disk" "pfsense_target_disk" {
+  name  = "pfsense-target-disk"
+  type  = "pd-ssd"
+  zone  = var.zone
+  size  = 20
+}
+
 resource "google_compute_instance" "pfsense_gateway" {
   name         = "pfsense-gateway"
   machine_type = "e2-medium"
@@ -5,36 +12,37 @@ resource "google_compute_instance" "pfsense_gateway" {
   can_ip_forward = true 
   tags         = ["pfsense-firewall"]
 
-  # WAN Interface (External facing)
   network_interface {
     network    = google_compute_network.untrusted_vpc.id
     subnetwork = google_compute_subnetwork.dmz_subnet.id
-    access_config {
-      # Leaving this empty assigns an Ephemeral Public IP
-    }
+    access_config {}
   }
 
-  # LAN Interface (Internal banking side)
   network_interface {
     network    = google_compute_network.trusted_vpc.id
     subnetwork = google_compute_subnetwork.app_subnet.id
   }
 
   scheduling {
-    preemptible       = true            
-    automatic_restart = false           
-    provisioning_model = "SPOT"         
+    preemptible                 = true            
+    automatic_restart           = false           
+    provisioning_model          = "SPOT"         
     instance_termination_action = "STOP" 
   }
 
   boot_disk {
     initialize_params {
       image = google_compute_image.pfsense_custom.self_link
-      size  = 20  
-      type  = "pd-ssd"
+      # Keep this small, it's just the installer
     }
   }
+
+  attached_disk {
+    source      = google_compute_disk.pfsense_target_disk.id
+    device_name = "target-disk"
+  }
+
   metadata = {
-  serial-port-enable = "1" 
-}
+    serial-port-enable = "1" 
+  }
 }
