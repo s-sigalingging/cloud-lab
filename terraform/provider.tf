3a8d9dfec8a589a4ef60740af8a628f56ae3e100
s-sigalingging <silveriussigalingging17@gmail.com>
2026-05-12 14:53:45 +0700

    trigger terraform apply

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "gcs" {
+    bucket = "cloud-lab-tf-state"
+    prefix = "terraform/state"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
