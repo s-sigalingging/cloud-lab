9a3d0e1de72d64b135d1b0fb1e9c7c475f282cdc
s-sigalingging <silveriussigalingging17@gmail.com>
2026-05-14 19:36:06 +0700

    add app VM

---
+++ terraform/banking_app.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "banking_app_server" {
+  name         = "banking-app-server"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
+      size  = 20
+      type  = "pd-ssd"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.trusted_vpc.id
+    subnetwork = google_compute_subnetwork.app_subnet.id
+  }
+
+  metadata = {
+    serial-port-enable = "1"
+  }
+}
