1593c4ad1eb11faced4b0cb468ee35565607d37c
s-sigalingging <silveriussigalingging17@gmail.com>
2026-05-16 12:34:08 +0700

    mofify routes

--- terraform/routes.tf
+++ terraform/routes.tf
@@ -1,6 +1,6 @@
 resource "google_compute_route" "app_to_pfsense" {
   name                   = "app-to-pfsense-route"
-  dest_range             = "0.0.0.0/0"
+  dest_range             = "10.10.10.0/24"
   network                = google_compute_network.trusted_vpc.id
   next_hop_instance      = google_compute_instance.pfsense_gateway.id
   next_hop_instance_zone = var.zone
