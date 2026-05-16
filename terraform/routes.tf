236fb0b314d1221ba55329d0d90ee6baa77f6d22
s-sigalingging <silveriussigalingging17@gmail.com>
2026-05-16 12:23:42 +0700

    add routes

--- terraform/routes.tf
+++ terraform/routes.tf
@@ -6,3 +6,11 @@ resource "google_compute_route" "app_to_pfsense" {
   next_hop_instance_zone = var.zone
   priority               = 100 
 }
+
+resource "google_compute_route" "trusted_vpc_internet_route" {
+  name             = "trusted-vpc-internet-route"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.trusted_vpc.name
+  next_hop_gateway = "default-internet-gateway"
+  priority         = 1000
+}
