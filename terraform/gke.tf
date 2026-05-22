615875c6deb5d5a3a8b0498e7cf1123bcab74a99
s-sigalingging <silveriussigalingging17@gmail.com>
2026-05-23 00:08:47 +0700

    test deploy

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -12,7 +12,6 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 2
 
-  # Enforce Network Policy (Enables internal cluster firewall rules)
   network_policy {
     enabled = true 
   }
