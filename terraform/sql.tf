d6870a00bbd8a8623a9648909bd6da88b3f9ab5c
s-sigalingging <silveriussigalingging17@gmail.com>
2026-05-27 00:25:45 +0700

    add cloudsql

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -2,7 +2,7 @@ resource "google_compute_subnetwork" "db_subnet" {
   name          = "banking-subnet"
   ip_cidr_range = "10.240.10.0/24" 
   region        = var.region
-  network       = trusted-vpc"        
+  network       = "trusted-vpc"        
   project       = var.project_id
 }
 
@@ -11,12 +11,12 @@ resource "google_compute_global_address" "private_ip_alloc" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16               
-  network       = trusted-vpc"
+  network       = "trusted-vpc"
   project       = var.project_id
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
-  network                 = trusted-vpc"        
+  network                 = "trusted-vpc"        
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
 }
